9eafae26aba309ce72de6c4fa27ec59e03d5d4fa
James Wiryo <jamesgiggs95@gmail.com>
2017-12-08 00:37:40 +0800

    Initial commit for NestedLists Problem

---
+++ Python/BasicDataTypes/NestedLists.py
@@ -0,0 +1,23 @@
+import math
+
+scoreList = []
+
+for _ in range(int(raw_input())):
+    name = raw_input()
+    score = float(raw_input())
+    scoreList.append([name, score])
+
+minScore = min(scoreList,  key=lambda x:x[1])[1]
+secondLowest = max(scoreList, key=lambda x:x[1])[1]
+secondLowestNameList = []
+
+for scoring in scoreList:
+    if minScore < scoring[1] <= secondLowest:
+        secondLowest = scoring[1]
+
+for list in scoreList:
+    if list[1] is secondLowest:
+        secondLowestNameList.append(list[0])
+
+for name in sorted(secondLowestNameList):
+    print name
