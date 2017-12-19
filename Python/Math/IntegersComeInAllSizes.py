cafe1465f9d0cad7213140d533dfa33bcd0a9f02
James Wiryo <jamesgiggs95@gmail.com>
2017-12-20 00:08:25 +0800

    Completed Integers Come In All Sizes Problem (integers in Python can be as big as byte memory)

---
+++ Python/Math/IntegersComeInAllSizes.py
@@ -0,0 +1,9 @@
+from __future__ import print_function;
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(raw_input())
+b = int(raw_input())
+c = int(raw_input())
+d = int(raw_input())
+
+print("{}".format(a**b + c**d))
