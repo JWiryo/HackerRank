fe701fb12552a1e41b0194c0d1b8873595a69f43
James Wiryo <jamesgiggs95@gmail.com>
2018-01-22 00:26:33 +0800

    Completed Day Of The Programmer Challenge

--- Algorithms/Implementations/DayOfTheProgrammer.py
+++ Algorithms/Implementations/DayOfTheProgrammer.py
@@ -3,7 +3,22 @@
 import sys
 
 def solve(year):
-    # Complete this function
+    if year == 1918:
+        return"26.09.{}".format(year)
+    elif isLeap(year):
+        return "12.09.{}".format(year)
+    else:
+        return "13.09.{}".format(year)
+
+
+def isLeap(year):
+    if year < 1917 and year % 4 == 0:
+        return True
+    elif year % 4 ==0 and (year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)):
+        return True
+    else:
+        return False
+
 
 year = int(raw_input().strip())
 result = solve(year)
