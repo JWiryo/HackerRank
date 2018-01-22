17de4c7a795d29f2d79c91f698874f7d07f600d0
James Wiryo <jamesgiggs95@gmail.com>
2018-01-23 01:08:02 +0800

    Initial Implementation of Sock Merchant Problem

---
+++ Algorithms/Implementations/SockMerchant.py
@@ -0,0 +1,11 @@
+#!/bin/python
+
+import sys
+
+def sockMerchant(n, ar):
+    # Complete this function
+
+n = int(raw_input().strip())
+ar = map(int, raw_input().strip().split(' '))
+result = sockMerchant(n, ar)
+print(result)
