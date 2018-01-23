477acd9eb0795a57b7484ae5883d84ea5f50384a
James Wiryo <jamesgiggs95@gmail.com>
2018-01-24 00:23:15 +0800

    Completed Sock Merchant Problem

--- Algorithms/Implementations/SockMerchant.py
+++ Algorithms/Implementations/SockMerchant.py
@@ -3,7 +3,17 @@
 import sys
 
 def sockMerchant(n, ar):
-    # Complete this function
+    count = 0
+    sock_dict = {}
+    for i in ar:
+        if i in sock_dict:
+            sock_dict[i] += 1
+        else:
+            sock_dict[i] = 1
+
+    for key, value in sock_dict.iteritems():
+        count += value/2
+    return count
 
 n = int(raw_input().strip())
 ar = map(int, raw_input().strip().split(' '))
