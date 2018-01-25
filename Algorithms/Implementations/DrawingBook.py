0f4538eb1f318d4fc78180140ea04a565edf2016
James Wiryo <jamesgiggs95@gmail.com>
2018-01-26 00:01:46 +0800

    Completed Drawing Book Problem

--- Algorithms/Implementations/DrawingBook.py
+++ Algorithms/Implementations/DrawingBook.py
@@ -1,9 +1,17 @@
 #!/bin/python
 
 import sys
+import math
 
 def solve(n, p):
-    # Complete this function
+    fromStart = math.ceil(abs(p-1)/2.0)
+    fromEnd = math.floor(abs(n-p)/2.0)
+    if p == n-1 and n % 2 == 0 and n != 2:
+        return 1
+    elif fromStart < fromEnd:
+        return int(fromStart)
+    else:
+        return int(fromEnd)
 
 n = int(raw_input().strip())
 p = int(raw_input().strip())
