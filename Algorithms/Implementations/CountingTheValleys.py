fb426d8fe7a7abcf4fbf420d4c3ee3dfde74498e
James Wiryo <jamesgiggs95@gmail.com>
2018-01-27 02:12:53 +0800

    Initial implementation of Counting The Valley Problem

---
+++ Algorithms/Implementations/CountingTheValleys.py
@@ -0,0 +1,12 @@
+#!/bin/python
+
+import sys
+
+def countingValleys(n, s):
+    # Complete this function
+
+if __name__ == "__main__":
+    n = int(raw_input().strip())
+    s = raw_input().strip()
+    result = countingValleys(n, s)
+    print result
