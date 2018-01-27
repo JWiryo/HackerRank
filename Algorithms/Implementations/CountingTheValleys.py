f618581352ae0b3bcdf9d7f2c6da9d3d1b06b2ea
James Wiryo <jamesgiggs95@gmail.com>
2018-01-28 01:06:40 +0800

    Completed Counting The Valleys Problem

--- Algorithms/Implementations/CountingTheValleys.py
+++ Algorithms/Implementations/CountingTheValleys.py
@@ -3,7 +3,16 @@
 import sys
 
 def countingValleys(n, s):
-    # Complete this function
+    curLevel = 0
+    valleyCount = 0
+    for step in list(s):
+        if step == 'U':
+            curLevel += 1
+        else:
+            curLevel -= 1
+        if curLevel == 0 and step == 'U':
+            valleyCount += 1
+    return valleyCount
 
 if __name__ == "__main__":
     n = int(raw_input().strip())
