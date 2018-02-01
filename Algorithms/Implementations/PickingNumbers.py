51a0e169d80efa60ffcbe78d043334a3fc08860e
James Wiryo <jamesgiggs95@gmail.com>
2018-02-02 01:15:02 +0800

    Completed Picking Numbers Problem

--- Algorithms/Implementations/PickingNumbers.py
+++ Algorithms/Implementations/PickingNumbers.py
@@ -3,7 +3,14 @@
 import sys
 
 def pickingNumbers(a):
-    # Complete this function
+    max_num = 0
+    for i in a:
+        j = a.count(i)
+        k = a.count(i-1)
+        j = j + k
+        if max_num < j:
+            max_num = j
+    return max_num
 
 if __name__ == "__main__":
     n = int(raw_input().strip())
