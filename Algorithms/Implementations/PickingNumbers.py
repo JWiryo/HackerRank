226a4f088aeba310520f7ec7eb851977e6f1037a
James Wiryo <jamesgiggs95@gmail.com>
2018-02-01 22:50:11 +0800

    Initial commit for Picking Numbers Problem

---
+++ Algorithms/Implementations/PickingNumbers.py
@@ -0,0 +1,12 @@
+#!/bin/python
+
+import sys
+
+def pickingNumbers(a):
+    # Complete this function
+
+if __name__ == "__main__":
+    n = int(raw_input().strip())
+    a = map(int, raw_input().strip().split(' '))
+    result = pickingNumbers(a)
+    print result
