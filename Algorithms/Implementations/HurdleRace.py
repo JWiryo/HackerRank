924c2a3b8f70d5449fb9e1dffe4b467d4078da84
James Wiryo <jamesgiggs95@gmail.com>
2018-02-05 00:29:33 +0800

    Initial implementation for Hurdle Race Problem

---
+++ Algorithms/Implementations/HurdleRace.py
@@ -0,0 +1,13 @@
+#!/bin/python
+
+import sys
+
+def hurdleRace(k, height):
+    # Complete this function
+
+if __name__ == "__main__":
+    n, k = raw_input().strip().split(' ')
+    n, k = [int(n), int(k)]
+    height = map(int, raw_input().strip().split(' '))
+    result = hurdleRace(k, height)
+    print result
